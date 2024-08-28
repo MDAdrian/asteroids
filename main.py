985e2e4067d051abc0c1b77c7c0433352fabe251
MDAdrian <adi.maniu33@gmail.com>
2024-08-28 11:26:24 +0300

    Added Space Ship

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 from constants import *
 import pygame
 
+from player import Player
+
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
 
     while(1):
         for event in pygame.event.get():
@@ -15,7 +24,9 @@ def main():
                 return
     
         screen.fill(color = "black")
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
---
+++ scratch.py
@@ -0,0 +1,4 @@
+mesaj = "fdsaqertlpo0hutredewqrtujhnm890ut567890bnmkl47890876543qazxcvbm67899tghjuyiopoiuuiopkmnbvcsdafghjkloiuytrewq2mnbvcxzasdfghjklpoiuytrewqgvcdxxxxzzasdfghjklopiuytrewqcvmnjhlooppjjjuytewqm,mjjnnnbv"
+mesaj2 = "salut tudor juiopijtreqasdfghjkjklmnbvcxzqwertyyuiop8bnmvbbbnmvghfuopghjcvbm xzxcccvvvbnmjhgfdddsaqwertyuuiopp vbnmhhbasdfcbvghfikj6yfhhvjnnbjugfujvjnn nvk,g,h.ljlnhlhogfojvjgjnvhfhgrfydhydhdhhchfhhcvhvujvugjjguuggubnmfnbchfhjgjjugunbbjbhjjhhhjn vjthgututjf"
+
+print("Mesaj1: " + mesaj + "\n" + "Mesaj 2: " + mesaj2 + "\n")
