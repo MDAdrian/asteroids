1cca7fa2d737fd841e45df884da64fe92cdb6e06
MDAdrian <adi.maniu33@gmail.com>
2024-08-30 15:45:26 +0300

    Added groups

--- player.py
+++ player.py
@@ -5,6 +5,8 @@ from constants import PLAYER_RADIUS, PLAYER_SPEED, PLAYER_TURN_SPEED
 
 class Player(CircleShape):
     
+    containers = ()
+
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.position = pygame.Vector2(x, y)
